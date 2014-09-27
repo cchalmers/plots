29a84904d54fc8e65895c5996831871e7666f353
Christopher Chalmers <c.chalmers@me.com>
2014-09-27 19:42:28 +0100

    Work on BarPlot.

--- src/Plots/Types/Bar.hs
+++ src/Plots/Types/Bar.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable    #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE ViewPatterns          #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell       #-}
@@ -8,28 +9,32 @@
 
 module Plots.Types.Bar
   ( BarPlot
+  , simpleBarPlot
     -- * Prism
   , _BarPlot
+
 
     -- * Lenses
   , barPlotBars
   , barPlotWidth
   , barPlotSpacing
+  , barPlotIsVerticle
   ) where
 
 import Control.Lens     hiding (transform, ( # ))
 import Data.Default
 import Data.Typeable
+import Data.Foldable (Foldable, foldMap, toList)
 import Diagrams.Prelude
 
 -- import Plots.Themes
 import Plots.Types
 
 data BarPlot b n = BarPlot
-  { _barPlotBars     :: [[n]]
-  , _barPlotWidth    :: n
-  , _barPlotSpacing  :: n
-  -- , _barPlotInterval :: n
+  { _barPlotBars     :: [(n,[n])] -- data for bars
+  , _barPlotWidth    :: n         -- total width of bars for one 'bit'
+  , _barPlotSpacing  :: n         -- gap between multibars in same value
+  , _barPlotIsVerticle :: Bool    -- whether the bars are verticle
   , _barPlotGeneric  :: GenericPlot b V2 n
   } deriving Typeable
 
@@ -41,43 +46,50 @@ makeLenses ''BarPlot
 instance HasGenericPlot (BarPlot b n) b where
   genericPlot = barPlotGeneric
 
--- instance HasStyle (BarPlot b) where
---   applyStyle sty = over (genericPlot . themeLineStyle)   (applyStyle sty)
---                  . over (genericPlot . themeMarkerStyle) (applyStyle sty)
---                  . over (genericPlot . themeFillStyle)   (applyStyle sty)
-
 instance (TypeableFloat n, Renderable (Path V2 n) b) => Default (BarPlot b n) where
   def = BarPlot
           { _barPlotBars     = []
-          , _barPlotWidth    = 0.9
+          , _barPlotWidth    = 0.5
           , _barPlotSpacing  = 0.1
-          -- , _barPlotInterval = 1
+          , _barPlotIsVerticle = True
           , _barPlotGeneric  = def
           }
 
--- drawBarPlot :: Renderable (Path R2) b => BarPlot b -> Diagram b R2
--- drawBarPlot bp = ifoldMap makeBar (bp^.barPlotBars)
---   where
---     bW = bp^.barPlotWidth
---     makeBar i [h] = rect bW h
---                       -- # applyStyle sty
---                       # centerX
---                       # alignB
---                       # translateX (fromIntegral i + 1)
--- 
---     makeBar i [h1,h2] = rect (bW / 2) h1
---                           # centerX
---                           # alignB
---                           # translateX (fromIntegral i + 1 - 0.15)
---                      <> rect (bW / 2) h2
---                           # fc orange
---                           # centerX
---                           # alignB
---                           # translateX (fromIntegral i + 1 + 0.15)
---     makeBar _ _ = mempty
+-- TODO: work out a nice way to get different colours for multi-bar plots.
+
+drawBarPlot :: (TypeableFloat n, Renderable (Path V2 n) b) => BarPlot b n -> Diagram b V2 n
+drawBarPlot bp = foldMap makeBar (bp^.barPlotBars)
+  where
+    tW = bp^.barPlotWidth
+    δ  = bp^.barPlotSpacing
+    --
+    makeBar (_,[]) = mempty
+    makeBar (x,bs) = ifoldMap mkBar bs
+      where
+        mkBar i h = rect w h
+                      # alignBL
+                      # translateX (x + fromIntegral i * (δ + w))
+        n = fromIntegral $ length bs
+        w = recip n * (tW - δ * (n - 1))
+
+    -- makeBar i [h1,h2] = rect (bW / 2) h1
+    --                       # centerX
+    --                       # alignB
+    --                       # translateX (fromIntegral i + 1 - 0.15)
+    --                  <> rect (bW / 2) h2
+    --                       # fc orange
+    --                       # centerX
+    --                       # alignB
+    --                       # translateX (fromIntegral i + 1 + 0.15)
+    -- makeBar _ _ = mempty
 
 -- instance (Typeable b, Renderable (Path R2) b) => Plotable (BarPlot b) b R2 where
 --   plot _r _ t = transform t . drawBarPlot
+
+simpleBarPlot :: (Renderable (Path V2 n) b, TypeableFloat n, Foldable f) => f n -> BarPlot b n
+simpleBarPlot (toList -> xs) = def & barPlotBars .~ imap f xs 
+  where
+    f i h = (fromIntegral i + 1, [h])
 
 _BarPlot :: Plotable (BarPlot b n) b => Prism' (Plot b V2 n) (BarPlot b n)
 _BarPlot = _Plot
