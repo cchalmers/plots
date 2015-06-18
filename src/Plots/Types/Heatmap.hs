fab2904d4f164cb52162c3feac5aff9489f33610
Christopher Chalmers <c.chalmers@me.com>
2015-06-18 13:44:49 +0100

    Add heatSquares function

--- src/Plots/Types/Heatmap.hs
+++ src/Plots/Types/Heatmap.hs
@@ -170,68 +170,24 @@ heatMap p0@(P (V2 xp yp)) extnt ns s f' = intHeatMap p0 ns s f
         x' = fromIntegral x
         y' = fromIntegral y
 
-
--- data GHeatMap v a n = GHeatMap
---   { _heatMapVector   :: v a
---   , _heatMapExtent   :: V2 Int  -- total width and height
---   , _heatMapStart    :: P2 n
---   , _heatMapSize     :: V2 n    -- width and height of each box
---   , _heatMapStyleMap :: a -> Style V2 n
---   } deriving Typeable
-
--- type instance V (GHeatMap v a n) = V2
--- type instance N (GHeatMap v a n) = n
-
--- makeLenses ''GHeatMap
-
--- -- instance HasGenericPlot (GHeatMap v a b n) where
--- --   genericPlot = heatMapGeneric
-
--- -- | Make a heat map from a vector. Each pixel has height and width 1 and the plot
--- mkHeatMap :: (Num n, GV.Vector v a)
---   => v a -> (Int,Int) -> (a -> Style V2 n) -> GHeatMap v a n
--- mkHeatMap v (w,h) f
---   = GHeatMap
---       { _heatMapVector   = v
---       , _heatMapExtent   = V2 w h
---       , _heatMapStart    = origin
---       , _heatMapSize     = V2 1 1
---       , _heatMapStyleMap = f
---       }
-
--- colourToStyle :: (Typeable n, Floating n) => AlphaColour Double -> Style V2 n
--- colourToStyle c = mempty # fcA c
-
-
--- mkGHeatMap :: (Typeable n, Floating n, P2Like Int p, GV.Vector v a, Foldable f)
---   => f (p, a) -> (Int,Int) -> a -> (a -> AlphaColour Double) -> GHeatMap v a n
--- mkGHeatMap xs (w,h) x0 f = mkHeatMap v (w,h) (colourToStyle . f)
---   where
---   v = GV.create $ do
---         mv <- GMV.replicate (w*h) x0
---         F.forM_ xs $ \(a, b) -> do
---           let P (V2 x y) = a ^. unpointLike
---               i          = y*h + x
-
---           GMV.write mv i b
-
---         return mv
-
-
-
--- --
--- -- instance (Typeable b, TypeableFloat n, Renderable (Path V2 n) b)
--- --     => Plotable (HeatMap b n) where
--- --   plot _ tv l t2 = drawHeatMap
--- --
--- --
--- --
--- -- simpleHeatMap :: (Renderable (Path V2 n) b, TypeableFloat n, Foldable f) => f n -> HeatMap b n
--- -- simpleHeatMap (toList -> xs) = def & HeatMapBars .~ imap f xs
--- --   where
--- --     f i h = (fromIntegral i + 1, [h])
--- --
--- -- _HeatMap :: Plotable (HeatMap b n) => Prism' (Plot b V2 n) (HeatMap b n)
--- -- _HeatMap = _Plot
--- --
--- --
+-- | Squares for heat map. Since some renderers leave gaps for adjacent
+--   blocks, squares are overlapped to avoid this.
+heatSquares :: (TypeableFloat n, Renderable (Path V2 n) b) => V2 Int -> (V2 Int -> Colour Double) -> QDiagram b V2 n Any
+heatSquares (V2 x y) f =
+  alaf Dual foldMap mk ps
+    # lwO 0
+-- foldMap is reversed using Dual so the squares are placed in the correct order
+  where
+    ps = [ V2 i j | i <- [0..x-1], j <- [0..y-1] ]
+    mk v@(V2 i j) =
+      rect w h
+        # alignBL
+        # translate (fromIntegral <$> v)
+        # fc (f v)
+        where
+          -- Squares that are not on the top left edge are slightly
+          -- bigger to remove phantom gaps
+          w | i == x    = 1
+            | otherwise = 1.5
+          h | j == y    = 1
+            | otherwise = 1.5
