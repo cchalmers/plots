976a76acc4a3c812e92a553504539a1acdeb2c60
Christopher Chalmers <c.chalmers@me.com>
2015-07-01 10:30:39 +0100

    Add scatter-2 example

---
+++ examples/scatter-2.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+import Plots
+import Plots.Axis
+import Plots.Themes
+import Plots.Types hiding (B)
+import Diagrams.Prelude
+import Diagrams.Backend.Rasterific
+
+mydata1 = [(1,3), (2,5.5), (3.2, 6), (3.5, 6.1)]
+mydata2 = mydata1 & each . _1 *~ 0.5
+mydata3 = [V2 1.2 2.7, V2 2 5.1, V2 3.2 2.6, V2 3.5 5]
+
+myaxis :: Axis B V2 Double
+myaxis = r2Axis &~ do
+  scatterPlot' mydata1 $ do
+    addLegendEntry "data 1"
+    plotColor .= purple
+    plotMarker %= scale 2
+  scatterPlotL "data 2" mydata2
+  scatterPlotL "data 3" mydata3
+
+  axisPlots . each . _ScatterPlot' . connectingLine .= True
+
+_ScatterPlot' :: Plotable (ScatterPlot v n) b => Traversal' (Plot' b v n) (ScatterPlot v n)
+_ScatterPlot' = _Plot'
+
+make :: Diagram B -> IO ()
+make = renderRasterific "examples/scatter-2.png" (mkWidth 600) . frame 20
+
+main :: IO ()
+main = make $ renderAxis myaxis
+
