b866bb5e81a16b241bce1d475d47a5a7d2267b52
Ajay Ramanathan <ajay.010293@gmail.com>
2015-07-16 19:34:22 +0530

    changes to example

--- examples/areagroup.hs
+++ examples/areagroup.hs
@@ -16,10 +16,9 @@ mydata3 = [V2 1.2 2.7, V2 2 5.1, V2 3.2 2.6, V2 3.5 5]
 
 myaxis :: Axis B V2 Double
 myaxis = r2Axis &~ do
-  
-
-_LinePlot' :: Plotable (LinePlot v n) b => Traversal' (Plot' b v n) (LinePlot v n)
-_LinePlot' = _Plot'
+     makearea' (fst foo1) (snd foo1)
+     makearea' (fst foo2) (snd foo2)
+     makearea' (fst foo3) (snd foo3)
 
 make :: Diagram B -> IO ()
 make = renderRasterific "test.png" (mkWidth 600) . frame 20
@@ -27,4 +26,6 @@ make = renderRasterific "test.png" (mkWidth 600) . frame 20
 main :: IO ()
 main = make $ renderAxis myaxis
 
-
+foo1 = ([(111.0,0.1),(140.0,1.2),(150.0,2.3)],"typeA")
+foo2 = ([(155.0,3.5),(167.0,5.1),(200.0,6.4),(211.0,7.5)],"typeB")
+foo3 = ([(191.0,5.8),(233.0,8.5),(250.0,9.1),(270.0,9.6)],"typeC")
