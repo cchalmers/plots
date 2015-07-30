ccc17cbefd1ba63c86bc194a60a700faea9a6f39
Ajay Ramanathan <ajay.010293@gmail.com>
2015-07-31 00:27:49 +0530

    added few changes to Heatmap

--- src/Plots/Types/Heatmap.hs
+++ src/Plots/Types/Heatmap.hs
@@ -23,38 +23,49 @@ module Plots.Types.Heatmap where
   -- ) where
 
 import           Control.Lens                    hiding (transform, ( # ))
+
 import           Data.Typeable
 import qualified Data.Vector.Unboxed as U
 import           Data.Vector.Unboxed ((!))
 import qualified Data.Foldable as F
+
 import           Diagrams.Prelude
 
 import           Plots.Themes
 import           Plots.Utils
-import Plots.Types
+import           Plots.Types
 
-data HeatMap n = HeatMap
-  { hmFunction :: Int -> Int -> Double
-  , hmExtent   :: V2 Int  -- total x and y boxes
-  , hmStart    :: P2 n
-  , hmSize     :: V2 n    -- width and height of each box
-  } deriving Typeable
+-- data HeatMap n = HeatMap
+--   { hmFunction :: Int -> Int -> Double
+--   , hmExtent   :: V2 Int  -- total x and y boxes
+--   , hmStart    :: P2 n
+--   , hmSize     :: V2 n    -- width and height of each box
+--   } deriving Typeable
 -- I'm still unsure of the "right" way to do this. On the one hand, by
 -- restricting it to int users can read off data from an array without
 -- any problems. On the other hand it's not an ideal representation for
 -- heat maps of functions. Maybe we need two primitives.
 
+type ColourRange = (Colour Double, Colour Double)
+   
+data HeatPlot n = HeatPlot
+  { hmData      =    [[n]]
+    hmColorMap  =    ColourRange
+  }
 type instance V (HeatMap n) = V2
 type instance N (HeatMap n) = n
 
-instance OrderedField n => Enveloped (HeatMap n) where
+instance OrderedField n => Enveloped (HeatPlot n) where
   getEnvelope HeatMap {..} = getEnvelope $
-    fromCorners hmStart (hmStart .+^ fmap fromIntegral hmExtent)
+    boundingBox (fromVertices [(p2 (0.5,0.5)), (p2 (0.5, ymax)), (p2 (xmax, ymax)), (p2 (ymax, 0.5))])
+    where 
+      ymax = (length hmData) + 0.5
+      xmax = (max [length x | x <- hmData]) + 0.5
 
 -- diagrams really needs a prim to deal with multiple objects like this.
 instance (Typeable b, TypeableFloat n, Renderable (Path V2 n) b)
-    => Plotable (HeatMap n) b where
-  renderPlotable s hm@HeatMap {..} pp =
+    => Plotable (HeatPlot n) b where
+  renderPlotable s HeatPlot {..} pp =
       F.foldMap mk ps
         # transform t
         # lwO 0.01
@@ -77,94 +88,17 @@ instance (Typeable b, TypeableFloat n, Renderable (Path V2 n) b)
       V2 vMin vMax = minmaxOf each (map (\(V2 i j) -> hmFunction i j) ps)
       normise v = (v - vMin) / (vMax - vMin)
 
+  defLegendPic HeatMap {..} pp
+      = square 5 # applyBarStyle pp
+myCircle :: Diagram B
+myCircle = rect 1 3 #fillTexture gradient #lw none
 
--- | Indexed traversal over the values of a heat map.
-heatPoints :: IndexedTraversal' (V2 Int) (HeatMap n) Double
-heatPoints f hm@HeatMap {..} = go 0 0 <&> \vs ->
-  let v       = U.fromListN (x*y) vs
-      hIx i j = v ! (x*j + i)
-  in  hm { hmFunction = hIx }
-  where
-    V2 x y = hmExtent
-    go !i !j
-      | i >= x    = go 0 (j+1)
-      | j >= y    = pure []
-      | otherwise = (:) <$> indexed f (V2 i j) (hmFunction i j) <*> go (i+1) j
-{-# INLINE heatPoints #-}
-
--- instance HasGenericPlot (GHeatMap v a b n) where
---   genericPlot = heatMapGeneric
-
--- | Make a heat map from a vector. Each pixel has height and width 1 and the plot
--- mkHeatMap :: (Num n, GV.Vector v a)
---   => v a -> (Int,Int) -> (a -> Style V2 n) -> GHeatMap v a n
--- mkHeatMap v (w,h) f
---   = GHeatMap
---       { _heatMapVector   = v
---       , _heatMapExtent   = V2 w h
---       , _heatMapStart    = origin
---       , _heatMapSize     = V2 1 1
---       , _heatMapStyleMap = f
---       }
-
-colourToStyle :: (Typeable n, Floating n) => AlphaColour Double -> Style V2 n
-colourToStyle c = mempty # fcA c
-
-
--- mkGHeatMap :: (Typeable n, Floating n, P2Like Int p, GV.Vector v a, Foldable f)
---   => f (p, a) -> (Int,Int) -> a -> (a -> AlphaColour Double) -> GHeatMap v a n
--- mkGHeatMap xs (w,h) x0 f = mkHeatMap v (w,h) (colourToStyle . f)
---   where
---   v = GV.create $ do
---         mv <- GMV.replicate (w*h) x0
---         F.forM_ xs $ \(a, b) -> do
---           let P (V2 x y) = a ^. unpointLike
---               i          = y*h + x
-
---           GMV.write mv i b
-
---         return mv
-
--- data HeatMap
-
-intHeatMap
-  :: P2 n   -- ^ start
-  -> V2 Int -- ^ extent
-  -> V2 n   -- ^ size of each box
-  -> (Int -> Int -> Double) -- ^ mapping function
-  -> HeatMap n
-intHeatMap p0 x s f =
-  HeatMap
-    { hmFunction = f
-    , hmExtent   = x
-    , hmStart    = p0
-    , hmSize     = s
-    }
-
-  -- { hmFunction :: Int -> Int -> Double
-  -- , hmExtent   :: V2 Int  -- total x and y boxes
-  -- , hmStart    :: P2 n
-  -- , hmSize     :: V2 n    --
-
-heatMap
-  :: Fractional n
-  => P2 n   -- ^ start
-  -> V2 n   -- ^ extent
-  -> V2 Int -- ^ number of boxes
-  -> V2 n   -- ^ size of each box
-  -> (n -> n -> Double) -- ^ mapping function
-  -> HeatMap n
-heatMap p0@(P (V2 xp yp)) extnt ns s f' = intHeatMap p0 ns s f
-  where
-    V2 dx dy = extnt / fmap fromIntegral ns
-    f x y = f' (xp + x' * dx) (yp + y' * dy)
-      where
-        x' = fromIntegral x
-        y' = fromIntegral y
+gradient = mkLinearGradient stops ((-1) ^& (-1)) (1 ^& 1) GradPad
+stops =  mkStops [(teal, 0, 1),(orange, 1, 1)]
 
 -- | Squares for heat map. Since some renderers leave gaps for adjacent
 --   blocks, squares are overlapped to avoid this.
-heatSquares :: (TypeableFloat n, Renderable (Path V2 n) b) => V2 Int -> (V2 Int -> Colour Double) -> QDiagram b V2 n Any
+heatSquares :: (TypeableFloat n, Renderable (Path V2 n) b) => n -> ColourRange -> QDiagram b V2 n Any
 heatSquares (V2 x y) f =
   alaf Dual F.foldMap mk ps
     # lwO 0
@@ -183,3 +117,112 @@ heatSquares (V2 x y) f =
             | otherwise = 1.5
           h | j == y    = 1
             | otherwise = 1.5
+
+mkHeatPlot :: (PointLike v n p, Additive v, TypeableFloat n) => (n -> p) -> ParametricPlot v n
+mkParametricPlot f
+  = HeatPlot
+        { hmData      =    [[n]]
+          hmColorMap  =    ColourRange
+        }
+
+------------------------------------------------------------------------
+-- Heatmap Lenses
+------------------------------------------------------------------------
+
+class HasHeatmap a v n | a -> v n where
+  heatmp :: Lens' a (HeatPlot v n)
+
+  setArrowOpts :: Lens' a ColourRange
+  setArrowOpts = heatmp . lens hmColorMap (\s b -> (s {hmColorMap = b}))
+
+instance HasVector (HeatPlot v n) v n where
+  heatmp = id
+
+instance HasVector (PropertiedPlot (HeatPlot v n) b) v n where
+  heatmp = _pp
+
+------------------------------------------------------------------------
+
+-- | Indexed traversal over the values of a heat map.
+-- heatPoints :: IndexedTraversal' (V2 Int) (HeatMap n) Double
+-- heatPoints f hm@HeatMap {..} = go 0 0 <&> \vs ->
+--   let v       = U.fromListN (x*y) vs
+--       hIx i j = v ! (x*j + i)
+--   in  hm { hmFunction = hIx }
+--   where
+--     V2 x y = hmExtent
+--     go !i !j
+--       | i >= x    = go 0 (j+1)
+--       | j >= y    = pure []
+--       | otherwise = (:) <$> indexed f (V2 i j) (hmFunction i j) <*> go (i+1) j
+-- {-# INLINE heatPoints #-}
+
+-- instance HasGenericPlot (GHeatMap v a b n) where
+--   genericPlot = heatMapGeneric
+
+-- | Make a heat map from a vector. Each pixel has height and width 1 and the plot
+-- mkHeatMap :: (Num n, GV.Vector v a)
+--   => v a -> (Int,Int) -> (a -> Style V2 n) -> GHeatMap v a n
+-- mkHeatMap v (w,h) f
+--   = GHeatMap
+--       { _heatMapVector   = v
+--       , _heatMapExtent   = V2 w h
+--       , _heatMapStart    = origin
+--       , _heatMapSize     = V2 1 1
+--       , _heatMapStyleMap = f
+--       }
+
+-- colourToStyle :: (Typeable n, Floating n) => AlphaColour Double -> Style V2 n
+-- colourToStyle c = mempty # fcA c
+
+
+-- mkGHeatMap :: (Typeable n, Floating n, P2Like Int p, GV.Vector v a, Foldable f)
+--   => f (p, a) -> (Int,Int) -> a -> (a -> AlphaColour Double) -> GHeatMap v a n
+-- mkGHeatMap xs (w,h) x0 f = mkHeatMap v (w,h) (colourToStyle . f)
+--   where
+--   v = GV.create $ do
+--         mv <- GMV.replicate (w*h) x0
+--         F.forM_ xs $ \(a, b) -> do
+--           let P (V2 x y) = a ^. unpointLike
+--               i          = y*h + x
+
+--           GMV.write mv i b
+
+--         return mv
+
+-- data HeatMap
+
+-- intHeatMap
+--   :: P2 n   -- ^ start
+--   -> V2 Int -- ^ extent
+--   -> V2 n   -- ^ size of each box
+--   -> (Int -> Int -> Double) -- ^ mapping function
+--   -> HeatMap n
+-- intHeatMap p0 x s f =
+--   HeatMap
+--     { hmFunction = f
+--     , hmExtent   = x
+--     , hmStart    = p0
+--     , hmSize     = s
+--     }
+
+  -- { hmFunction :: Int -> Int -> Double
+  -- , hmExtent   :: V2 Int  -- total x and y boxes
+  -- , hmStart    :: P2 n
+  -- , hmSize     :: V2 n    --
+
+-- heatMap
+--  :: Fractional n
+--   => P2 n   -- ^ start
+--  -> V2 n   -- ^ extent
+--  -> V2 Int -- ^ number of boxes
+--  -> V2 n   -- ^ size of each box
+--  -> (n -> n -> Double) -- ^ mapping function
+--  -> HeatMap n
+-- heatMap p0@(P (V2 xp yp)) extnt ns s f' = intHeatMap p0 ns s f
+--   where
+--     V2 dx dy = extnt / fmap fromIntegral ns
+--     f x y = f' (xp + x' * dx) (yp + y' * dy)
+--       where
+--         x' = fromIntegral x
+--         y' = fromIntegral y
