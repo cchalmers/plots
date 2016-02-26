dfd803fbe9e12e5092b83f105d7666feff0e24d6
Christopher Chalmers <c.chalmers@me.com>
2016-02-26 21:16:22 +0000

    Remove unused import in boxplotpoint example

--- examples/boxplotpoint.hs
+++ examples/boxplotpoint.hs
@@ -1,24 +1,12 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 import Plots
-import Plots.Axis
-import Plots.Types hiding (B)
-import Plots.Themes
-import Plots.Utils
-
-import Data.List
 
 import Diagrams.Prelude
-import Diagrams.Backend.Rasterific
-import Diagrams.Backend.CmdLine
-
-import Data.Array
-import Data.Monoid.Recommend
-
-import Dataset
+import Diagrams.Backend.Rasterific.CmdLine
 
 alldata = zip (zip petalLength petalWidth) species
-  
+
 mydata1 = foobarsingle alldata "setosa"
 mydata2 = foobarsingle alldata "versicolor"
 mydata3 = foobarsingle alldata "virginica"
@@ -37,7 +25,7 @@ myaxis = r2Axis &~ do
       plotColor .= teal
       fillOpacity .= 0.5
       addLegendEntry (snd mydata1)
-      
+
     boxPlot' (fst mydata2) $ do
       plotColor .= orange
       fillOpacity .= 0.5
@@ -48,15 +36,10 @@ myaxis = r2Axis &~ do
       fillOpacity .= 0.5
       addLegendEntry (snd mydata3)
 
-    scatterPlot' (fst mydata1) $ do
-      plotColor .= teal
+    scatterPlot (fst mydata1) $ plotColor .= teal
+    scatterPlot (fst mydata2) $ plotColor .= orange
+    scatterPlot (fst mydata3) $ plotColor .= purple
 
-    scatterPlot' (fst mydata2) $ do
-      plotColor .= orange
-
-    scatterPlot' (fst mydata3) $ do
-      plotColor .= purple
- 
 make :: Diagram B -> IO ()
 make = renderRasterific "test.png" (mkWidth 1000) . frame 20
 
